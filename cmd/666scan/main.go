aaeb05a335458f4e33765cbae638225ceac32b14
_lavalamp <cegrayson3@gmail.com>
2018-11-19 15:38:08 -0800

    Empty out Bloom filter history when new target network.

--- cmd/666scan/main.go
+++ cmd/666scan/main.go
@@ -173,11 +173,15 @@ func main() {
 		if mostRecentNetworkString == "" {
 			log.Printf("No prior record of a scanned network exists. Resetting state machine to scan %s appropriately.", targetNetwork)
 		} else {
-			log.Printf("Target network (%s) is not the most recently scanned network (%s). Resetting state machine accordingly.", targetNetwork, mostRecentNetworkString)
+			log.Printf("Target network (%s) is not the most recently scanned network (%s). Resetting state machine and Bloom filter accordingly.", targetNetwork, mostRecentNetworkString)
 		}
 		err := statemachine.ResetStateFile(conf.GetStateFilePath())
 		if err != nil {
 			log.Fatalf("Error thrown when resetting state file: %e", err)
+		}
+		_, _, err = fs.DeleteAllFilesInDirectory(conf.GetBloomDirPath(), []string{})
+		if err != nil {
+			log.Fatalf("Error thrown when deleting Bloom directory files (path '%s'): %e", conf.GetBloomDirPath(), err)
 		}
 		err = data.WriteMostRecentTargetNetwork(targetNetwork, &conf)
 		if err != nil {
